a950a40447f91f7a3bf0a502e11001fa63741dd6
SamSamKDBX <samuel.bour@proton.me>
2026-06-30 20:06:30 +0200

    implementing tests for Board.GetPiece() method

--- Assets/Tests/Board/GetPieceTests.cs
+++ Assets/Tests/Board/GetPieceTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using NUnit.Framework;
 using UnityEngine;
@@ -6,10 +7,58 @@ using UnityEngine.TestTools;
 public class GetPieceTests
 {
     private Board _board = new GameObject().AddComponent<Board>();
+    private King _piece = new GameObject().AddComponent<King>();
 
     [SetUp]
     public void SetUpBoard()
     {
         _board.Initialize();
+        _piece.Initialize(Colors.WHITE, new SquareV2(0, 0));
+    }
+
+    [Test]
+    public void ShouldGetPiece()
+    {
+        // GIVEN
+        SquareV2 target = new SquareV2(1, 0);
+        _board.PutPiece(_piece, target);
+        // WHEN
+        PieceV2 actualPiece = _board.GetPiece(target);
+        // THEN
+        Assert.AreEqual(_piece, actualPiece);
+    }
+
+    [Test]
+    public void ShouldNotGetPieceAtNullTarget()
+    {
+        // GIVEN
+        SquareV2 target = new SquareV2(1, 0);
+        _board.PutPiece(_piece, target);
+        // WHEN
+        target = null;
+        // THEN
+        Assert.Throws<ArgumentNullException>(() => _board.GetPiece(target));
+    }
+
+    [Test]
+    public void ShouldNotGetPieceAtOutTarget()
+    {
+        // GIVEN
+        SquareV2 target = new SquareV2(1, 0);
+        _board.PutPiece(_piece, target);
+        // WHEN
+        target = new SquareV2(100, 100);
+        // THEN
+        Assert.Throws<InvalidOperationException>(() => _board.GetPiece(target));
+    }
+
+    [Test]
+    public void ShouldNotGetUnplacedPiece()
+    {
+        // GIVEN
+        SquareV2 target = new SquareV2(1, 0);
+        // WHEN
+        // THEN
+        Assert.Throws<InvalidOperationException>(() => _board.GetPiece(target));
     }
 }
